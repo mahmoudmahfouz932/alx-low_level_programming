b380ebb6949c5edc6c9a7817084d558fda98c96e
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-08 10:18:41 -0700

    edit size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 /**
- * main -Entry point
+ *main - Entry point
  *
+ *Using size of function to print size of variable types
  *Return: Always 0(success)
  */
 int main(void)
 {
 
-	printf("Size of a char:%u byte(s)\n", sizeof(char));
-	printf("Size of a int:%u byte(s)\n", sizeof(int));
-	printf("Size of a long int:%u byte(s)\n", sizeof(long int));
-	printf("Size of a long long int:%u byte(s)\n", sizeof(long long int));
-	printf("Size of a float:%u byte(s)\n", sizeof(float));
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of a int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
 	return (0);
+}
