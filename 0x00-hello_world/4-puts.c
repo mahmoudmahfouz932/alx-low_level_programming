52238a1b4e5c8cd032869a06eb76712ad9351cd6
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-08 09:42:43 -0700

    edit 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-int main(void)
- {	
-		//starting my app from here
+/**
+ * main -Entry point
+ *
+ *Return: Always 0(success)
+ */
+int main(void) 
+{	
+	
 	char mystr[] = "\"Programming is like building a multilingual puzzle";
 			
 	puts(mystr);
 	return (0);		//ending my app here
- } 
+} 
