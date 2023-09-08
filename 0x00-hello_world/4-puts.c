184b6a7ac241565059d4b296e1efc68c2d374df6
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-08 09:33:59 -0700

    edit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-int main(){
-
-	char mystr[]= "Programming is like building a multilingual puzzle";
+int main(void)
+ {	
+		//starting my app from here
+	char mystr[] = "\"Programming is like building a multilingual puzzle";
+			
 	puts(mystr);
-	return 0;
-}
+	return (0);		//ending my app here
+ } 
