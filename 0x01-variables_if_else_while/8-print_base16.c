f554e84010de99913994fb83308688d02b1a5688
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-09 04:28:27 -0700

    sfd

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*
+*Return: Always 0(success)
+*/
+int main(void)
+{
+	char i[16] = ['0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f'];
+
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
