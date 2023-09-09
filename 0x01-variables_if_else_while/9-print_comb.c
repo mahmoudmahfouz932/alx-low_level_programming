614490ba44e3eb8058f1deb51174527ef273dc57
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-09 04:47:50 -0700

    combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*
+*Return: Always 0(success)
+*/
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c == '9')
+		{
+			putchar(c);
+			continue;
+		}
+		putchar(c);
+		putchar(', ');
+	}
+	putchar('\n');
+	return (0);
+}
