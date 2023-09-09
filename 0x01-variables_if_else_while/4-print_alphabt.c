2fa4701b0c762f57b9ddc08e88c10583c4e98668
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-09 03:40:32 -0700

    continue

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*
+*Return: Always 0(success)
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'q' || i == 'e')
+		{
+			i++;
+			continue;
+		}
+		putchar(i);
+	}
+	return (0);
+}
