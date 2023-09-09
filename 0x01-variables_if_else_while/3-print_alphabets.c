1909ceb4c49d893d81fd6e0998f251556a4e1598
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-09 03:31:03 -0700

    for

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*
+*Return: Always 0(success)
+*/
+int main(void)
+{
+	char c;
+	char a
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (a = 'A'; a <= 'Z'; a++ )
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
