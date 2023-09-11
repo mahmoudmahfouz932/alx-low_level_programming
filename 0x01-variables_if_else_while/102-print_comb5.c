5743e6f672070362eef966a3f53564e91a13ed1f
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-11 06:36:32 -0700

    last one

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*
+*Return: Always 0(success)
+*/
+int main(void)
+{
+	int i, x, z, y;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			putchar((i % 10) + '0');
+			putchar((x % 10) + '0');
+			putchar(' ');
+			for (z =0; z < 9; z++)
+			{
+				for (y = z + 1; y < 10; y++)
+				{
+					putchar((z % 10) + '0');
+					putchar((y % 10) + '0');
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+}
+	putchar('\n');
+	return (0);
+}
