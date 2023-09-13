aa92fc73cbd4d83817d34422d96d39f64eb28d44
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-13 14:36:07 -0700

    add two num ~

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*add -Entry
+*add function used to add to numbers
+*@n1: The character to be checked
+*@n2: The character to be checked
+*Return: value 
+*/
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+*times_table -Entry
+*times-table using for loop to print table 9 times
+**Return: void
+*/
+void times_table(void)
+{
+	
+}
