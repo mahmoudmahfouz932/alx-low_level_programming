c8107f42a2a5f201a830525cea059e8992cf2424
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-13 05:42:24 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_islower -Entry
+*_islower to check if lowercase or not
+*Return: Always success 0
+*/
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
