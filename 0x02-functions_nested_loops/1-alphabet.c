a803967e058733dda51cef8b72abe21e203e29ee
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-13 04:25:14 -0700

    add

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*main -Entry point
+*
+*Return: Always 0(success)
+*/
+int mani(void)
+{
+	int i;
+	char c;
+
+	print_alphabet_x10()
+	{	for (i = 0; i <= 9; i++)
+		{
+			for (c = 'a'; c < 'z'; c++)
+			{
+				_putchar(c);
+			}
+		}
+	_putchar('\n');
+	return (o);	
+}
