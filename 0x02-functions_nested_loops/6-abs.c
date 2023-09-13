b32c6652d54c66cbc70f3a890538192785844d04
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-13 06:14:11 -0700

    abs value of num

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main."
+
+/**
+*_abs -Entry
+*_abs  to return absolut value of integer
+*@n: The character to be checked
+*Return: Always success 0
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
