92f30f1d96558fcadc0c27c31eeb78d0d2a394d5
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-13 15:01:19 -0700

    edit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,19 +11,23 @@ void print_to_98(int n)
 	{
 		for (n = n; n <= 98; n++)
 		{
-			_putchar(n);
+			_putchar(n + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
 	}
-	else if (n >= 98)
+	else if (n > 98)
 	{
 		for (n = n; n >= 98; n--)
 		{
-			_putchar(n);
+			_putchar(n + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
+	}
+	else
+	{
+		_putchar(n + '0');
 	}
 	_putchar('\n');
 }
