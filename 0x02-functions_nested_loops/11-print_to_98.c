d612aacab01ea21cb83b4ed179f7aa8789b76b97
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-13 14:55:54 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+*print_to_98 -Entry
+*print_to_98 to print all natural numbers to 98
+*@n: The character to be checked
+*Return: void
+*/
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (n = n; n <= 98; n++)
+		{
+			_putchar(n + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else if (n >= 98)
+	{
+		for (n = n; n >= 98; n--)
+		{
+			_putchar(n + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
