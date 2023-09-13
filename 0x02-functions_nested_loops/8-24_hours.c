670786a693b9aca622cd3c1856aa95d8541bfff3
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-13 09:37:20 -0700

    24hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,14 +10,14 @@ void jack_bauer(void)
 	float i;
 	float x;
 
-	for (i = 00.00; i <= 23; i++)
+	for (i = 0; i <= 23; i++)
 	{
-		for (x = 00.00; x <= 59; x++)
+		for (x = 0; x <= 59; x++)
 		{
 			_putchar(i + '0');
 			_putchar(':');
 			_putchar(x + '0');
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
