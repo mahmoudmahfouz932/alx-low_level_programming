f5009510d4e548b993abd2c2b9fdcb7b577097e2
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-13 09:55:35 -0700

    edit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,17 +7,25 @@
 */
 void jack_bauer(void)
 {	
-	float i;
-	float x;
+	int i, x, y, z;
 
-	for (i = 0; i <= 23; i++)
+	for (i = 0; i <= 2; i++)
 	{
-		for (x = 0; x <= 59; x++)
-		{
-			_putchar(i + '0');
-			_putchar(':');
-			_putchar(x + '0');
-			_putchar('\n');
-		}
+		for (x = 0; x <= 9)
+			if ((i <= 1 && x <= 9 ) || (i == 2 && x <= 3))
+			{
+				for (y = 0; y <= 5; x++)
+				{
+					for (z = 0; z <= 9; z++)
+					{
+						_putchar(i + '0');
+						_putchar(x + '0');
+						_putchar(':');
+						_putchar(y + '0');
+						_putchar(z + '0');
+						_putchar('\n');
+					}
+				}
+			}
 	}
 }
