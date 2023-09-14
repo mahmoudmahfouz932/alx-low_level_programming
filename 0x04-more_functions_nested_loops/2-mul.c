c124494d67ff49b7dbfc451b335f3ab326df9ea1
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-13 23:41:58 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*mul -Entry
+*mul function used to add to numbers
+*@a: The character to be checked
+*@b: The character to be checked
+*Return: value
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
