5f7490d6f8841cf09e98eb9044eb2d8f7447f322
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-13 23:53:37 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_most_numbers -Entry
+*print_most_numbers using while loop from 0 to 9
+*@c: The character to be checked
+**Return: void
+*/
+void print_most_numbers(void)
+{
+	char c;
+
+	while (c >= '0' && c <= '9')
+	{
+		if(c == '2' || c == '4')
+		{
+			continue;
+		}
+		_putchar(c);
+	}
+	_putchar('\n');
+}
