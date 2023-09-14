5ee2c62e65c66385523048d49c743083169bf596
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-14 16:32:11 -0700

    edit 3

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,20 +3,19 @@
 /**
 *more_numbers -Entry
 *more_numbers using while loop from 0 to 9
-*@c: The character to be checked
 **Return: void
 */
 void more_numbers(void)
 {
-	char c = '0';
+	int c = 0;
 	int i = 0;
 
 	while (i <= 9)
 	{
 		i++;
-		while (c >= '0' && c <= '14')
+		while (c >= 0 && c <= 14)
 		{
-			_putchar(c);
+			_putchar(c + '0');
 			c++;
 		}
 	}
