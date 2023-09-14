4273e624aff54b1ac051797d1db2dd6e304f0de4
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-14 00:09:57 -0700

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_line -Entry
+*print_line using while loop to print n of lines
+*@n: The character to be checked
+**Return: void
+*/
+void print_line(int n)
+{
+	while (n >= 0)
+	{
+		_putchar('_');
+		n++;
+	}
+}
