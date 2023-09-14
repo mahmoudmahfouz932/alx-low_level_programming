2c07abd43dc76df85ce621ef0e9cd01c30e7b756
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-13 23:32:48 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_isupper -Entry
+*_upper to check if uppercase or not
+*@c: The character to be checked
+*Return: Always success 0
+*/
+int _isupper(int c);
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
