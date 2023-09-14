6b9cc3c90115bf7a42b5025ec2e0ca238bd762be
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-14 00:12:32 -0700

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_diagonal -Entry
+*print_diagonal using while loop to print n of lines
+*@n: The character to be checked
+**Return: void
+*/
+void print_diagonal(int n)
+{
+	while (n >= 0)
+	{
+		_putchar(92);
+		n++;
+	}
+}
