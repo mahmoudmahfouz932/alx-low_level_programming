d3913f5f2a73f683c70ff70f004c9a4fdab187be
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-16 03:50:51 -0700

    Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the first 52 fibonacci numbers
+  *
+  * Return: Nothing!
+  */
+int main(void)
+{
+	int i;
+	long x = 1, z = 2;
+
+	for (i = 0; i < 50; ++i)
+	{
+		if (i == 0)
+			printf("%ld", x);
+		else if (i == 1)
+			printf(", %ld", z);
+		else
+		{
+			z = z + x;
+			x = z - x;
+			printf(", %ld", k);
+		}
+	}
+
+	printf("\n");
+	return (0);
+}
