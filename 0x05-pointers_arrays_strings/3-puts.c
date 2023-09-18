69dc6e2b0dcf2a8f6558f9e1cf1916c841d8d930
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-18 06:21:59 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_puts -Entry
+*_puts to print string followed by new line
+*@str: The pointer used in function
+**Return: void
+*/
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*str != '\0')
+		printf(*str[i]);
+		i++;
+		str++;
+}
