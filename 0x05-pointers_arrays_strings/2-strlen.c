55b2668b644a4d05746958277f847c162f2cae53
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-18 04:29:22 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_strlen -Entry
+*_strlen to return leghth
+*@s: The pointer used in function
+**Return: Always success 0
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while  (i <= *s)
+	{
+		i++
+	}
+	return (i);
+
+}
