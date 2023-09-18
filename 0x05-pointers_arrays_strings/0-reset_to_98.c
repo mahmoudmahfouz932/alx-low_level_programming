899f505c69f307ca8dda7c76f22825d097204c96
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-18 04:05:30 -0700

    edit pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
 *reset_to_98 -Entry
 *reset_to_98 to reset the value useing pointer
-*@*n: The pointer used in function
+*@n: The pointer used in function
 **Return: void
 */
 void reset_to_98(int *n)
