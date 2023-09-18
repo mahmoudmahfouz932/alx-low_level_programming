0ce34cc30a4bc5faffd9c1d7696f5786741a8b03
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-18 04:12:00 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*reset_to_98 -Entry
+*reset_to_98 to reset the value useing pointer
+*@a: The pointer used in function
+*@b: The pointer used in function
+**Return: void
+*/
+void swap_int(int *a, int *b)
+{
+	int *ptr1 = *a;
+	int *ptr2 = *b;
+	*a = *ptr2;
+	*b = *ptr1;
+}
