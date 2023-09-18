199afb94584a3f5b094f91b5901b03c5e2b76b24
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-18 03:52:27 -0700

    rest pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*reset_to_98 -Entry
+*reset_to_98 to reset the value useing pointer
+*@*n: The pointer used in function
+**Return: void
+*/
+void reset_to_98(int *n)
+{
+	n = &n;
+	*n = 98;
+
+}
