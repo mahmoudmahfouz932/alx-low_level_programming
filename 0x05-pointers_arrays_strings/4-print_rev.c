1b7e72fdb249f7ac0d99d201015f46adedcc7ab2
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-18 06:46:52 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-*print_rev strlen -Entry
-*print_rev print string in revese
+*print_rev strlen - Entry
+*print_rev prints string in revese
 *@s: The pointer used in function
 **Return: Always success 0
 */
 void print_rev(char *s)
 {
-	while (*s != '\0')
+	while (*s <= '\0')
 	{
 		_putchar(*s);
 		s--;
