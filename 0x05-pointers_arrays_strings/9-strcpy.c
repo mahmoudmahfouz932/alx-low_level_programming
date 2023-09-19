b7abd3fb8752583af7ff6c70e7237b922128ff00
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-18 18:59:39 -0700

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+*print_array - Entry
+*print_array prints string in revese
+*@a: The array used in function
+*@n: The number of element to print
+**Return: void
+*/
+void print_array(int *a, int n)
+{
+	int i = 0;
+	int c = 0;
+
+	while (a[c] != '\0')
+	{
+		c++;
+	}
+	c = c - 1;
+	while (i < n)
+	{
+		if (i == n - 1)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+		printf("%d, ", a[i]);
+		i++;
+		}
+	}
+	printf("\n");
+}
