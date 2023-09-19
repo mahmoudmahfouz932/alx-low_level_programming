81bd33c431eb08cf181990fbc477523460b82598
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-18 18:45:31 -0700

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,16 +4,22 @@
 /**
 *print_array - Entry
 *print_array prints string in revese
-*@a: The pointers used in function
-*@b: The pointers used in function
+*@a: The array used in function
+*@n: The number of element to print
 **Return: void
 */
 void print_array(int *a, int n)
 {
 	int i = 0;
+	int c = 0;
 
-	while (i <= n)
+	while (*a != '\0')
+		c++;
+	c = c - 1;
+	while (i < n)
 	{
+		if (i == c)
+			printf("%d", a[i]);
 		printf("%d, ", a[i]);
 		i++;
 	}
