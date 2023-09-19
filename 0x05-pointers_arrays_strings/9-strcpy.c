ecfd23eaef78e3c33fe0f5b0951a860a0e6d1b50
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-19 05:08:05 -0700

    &(date)

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,36 +10,15 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
-	int z = 0;
-	int x = 0;
-	int y = 0;
-	char cpy;
 
 	while (src[i] != '\0')
 	{
 		i++;
 	}
 	i = i - 1;
-	while (i > z)
+	while (i >= 0)
 	{
-		cpy = src[i];
-		src[i] = src[z];
-		src[z] = cpy;
+		dest[i] = src[i];
 		i--;
-		z++;
 	}
-	while (cpy[x] != '\0')
-        {
-                x++;
-        }
-        x = x - 1;
-        while (x > y)
-        {
-                dest = cpy[x];
-                cpy[x] = cpy[y];
-                cpy[y] = dest;
-                x--;
-                y++;
-        }
-
 }
