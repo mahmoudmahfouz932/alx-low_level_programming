29ebada7a42b8628a2280a195717c83e1f7fe8e5
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-19 05:00:20 -0700

    &(date)

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,33 +1,45 @@
 #include "main.h"
 
 /**
-*print_array - Entry
-*print_array prints string in revese
-*@a: The array used in function
-*@n: The number of element to print
+**_strcpy - Entry
+**_strcpy to revese array
+*@*dest: The new array copy
+*@*src: The array want to copy
 **Return: void
 */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
-	int c = 0;
+	int z = 0;
+	int x = 0;
+	int y = 0;
+	int cpy;
 
-	while (a[c] != '\0')
+	while (src[i] != '\0')
 	{
-		c++;
-	}
-	c = c - 1;
-	while (i < n)
-	{
-		if (i == n - 1)
-		{
-			printf("%d", a[i]);
-		}
-		else
-		{
-		printf("%d, ", a[i]);
 		i++;
-		}
 	}
-	printf("\n");
+	i = i - 1;
+	while (i > z)
+	{
+		cpy = src[i];
+		src[i] = src[z];
+		src[z] = cpy;
+		i--;
+		z++;
+	}
+	while (cpy[x] != '\0')
+        {
+                x++;
+        }
+        x = x - 1;
+        while (x > y)
+        {
+                dest = cpy[x];
+                cpy[x] = cpy[y];
+                cpy[y] = dest;
+                x--;
+                y++;
+        }
+
 }
