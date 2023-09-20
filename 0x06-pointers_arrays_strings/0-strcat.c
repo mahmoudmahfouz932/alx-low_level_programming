7ab2e650d9bdac57b269bae6d65e2a430696e1c2
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-20 13:39:47 -0700

    &(date)

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,11 +16,11 @@ char *_strcat(char *dest, char *src)
 		len1++;
 	}
 	len1 = len1 + 1;
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; src[i] != 0; i++)
 	{
 		dest[len1] = src[i];
 		len1++;
 	}
-	dest[len1 + 1] = '\0';
+	dest[len1] = '\0';
 	return (dest);
 }
