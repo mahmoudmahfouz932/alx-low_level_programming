3a03b48c21943e5562a7e69f1eba242fda394621
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-20 13:19:30 -0700

    &(date)

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * *_strcat -Entry point
+ * *_strcat to append two Strings
+ * @dest: The new array copy
+ * @src: The array want to copy
+ * Return: char*
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len1 = 0;
+	int len2 = 0;
+	int i = 0;
+
+	while (dest[len1] != '\0')
+	{
+		len1++;
+	}
+	while (src[len2] != '\0')
+	{
+		len2++;
+	}
+	dest[len1 + len2] = '\0';
+	for (i = 0; len1 < len1 + len2; i++)
+	{
+		dest[len1]=src[i];
+		len1++;
+	}
+	return (dest);
+}
