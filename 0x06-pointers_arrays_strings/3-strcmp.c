228a9d5b193ef5b1cf155b269987ab2688158c73
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-21 11:23:05 -0700

    &(date)

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,34 +8,7 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int len1 = 0, len2 = 0, len, z;
+	int i = 0;
 
-	while (s1[eln1])
-	{
-		len1++;
-	}
-	while (s2[eln2])
-        {
-                len2++;
-        }
-	if (len1 >= len2)
-	{
-		len = len1;
-	}
-	else
-		len = len2
-	for (i = 0; i < len; i++)
-	{
-		if (s1[i] == s2[i])
-		{
-			continue;
-		}
-		else
-		{
-			z = s1[i] - s2[i];
-			break;
-		}
-		i++;
-	}
-	return (z);
+	return (s1[i] - s2[i]);
 }
