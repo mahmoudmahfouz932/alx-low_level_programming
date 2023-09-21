b84057a5855d227172cc38b8df58740a3c835126
Mahmoud_Mahfouz <mahmoudmahfouz123@gmail.com>
2023-09-21 11:56:09 -0700

    &(date)

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,14 +4,14 @@
 *reverse_array - Entry
 *reverse_array to revese array
 *@a: The pointer used in function
-*@: Thenumber of elements
+*@n: Thenumber of elements
 **Return: void
 */
 void reverse_array(int *a, int n)
 {
-    int i = 0;
+	int i = 0;
 	int z = 0;
-	int* rs;
+	int rs;
 
 	while (i < n)
 	{
