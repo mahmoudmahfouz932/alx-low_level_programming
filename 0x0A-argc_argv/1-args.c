772fd2192a64092ded2f39438d0ebfb9e204d406
Mahmoud Mahfouz <mahmoudmahfouz123@gmail.com>
2023-10-27 16:15:21 -0700

    create program print numer of arrguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+$include "main.h"
+/**
+  * main - Prints its name
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
