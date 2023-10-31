02dbff96a807073d646021c7eb59c7e566f44a82
Mahmoud Mahfouz <mahmoudmahfouz123@gmail.com>
2023-10-31 14:07:15 -0700

    add strup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * create_array - Create array
+  * @size: size of array
+  * @c: array elements
+  *
+  * Return: char
+  */
+char *_strdup(char *str)
+{
+
+}
