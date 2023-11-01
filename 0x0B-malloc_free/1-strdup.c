d41481ba071459b24760fecdd3e716e1f21038e3
Mahmoud Mahfouz <mahmoudmahfouz123@gmail.com>
2023-11-01 02:41:04 -0700

    add new task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,19 +12,17 @@ char *_strdup(char *str)
 	char *s;
 	int i = 0;
 	int c;
-	
+
 	if (str == NULL)
 		return (NULL);
 	while (str[i])
 		i++;
 
-	s = malloc((sizeof(char) * i) + 1);
-	
+	s = malloc((sizeof(char) * i) + 1);	
 	if (s == NULL)
 		return (NULL);
 	for (c = 0; c < i; c++)
 		s[c] = str[c];
 	s[i] = '\0';
 	return (s);
-	
 }
