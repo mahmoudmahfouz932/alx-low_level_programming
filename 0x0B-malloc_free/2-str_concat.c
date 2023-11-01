0fedf3b577b5298066a93474ab9aa3491d83ac7b
Mahmoud Mahfouz <mahmoudmahfouz123@gmail.com>
2023-11-01 03:01:00 -0700

    new  project

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * str_concat - Concatenates two strings of any size
+  * @s1: the first string to concatenate
+  * @s2: the second string to concatenate
+  *
+  * Return: the two strings concatenated
+  */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0;
+	int z = 0;
+	int x = 0;
+	int c;
+	char *s;
+
+	if (s1 == NULL)
+		return (NULL);
+	if (s2 == NULL)
+		return (NULL);
+	while (s1[i])
+		i++;
+	while (s2[z])
+		z++;
+	s = malloc((sizeof(char) * (i + z)) + 1);
+	if (s == NULL)
+		return (NULL);
+	for (c = 0; c < i; c++)
+		s[c] = s1[c];
+	c++;
+	while (c < (i + z))
+	{
+		s[c] = s2[x];
+		c++;
+		x++;
+	}
+	s[i + z] = '\0';
+	return (s);
+}
