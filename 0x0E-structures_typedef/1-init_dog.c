f13a924a349753ebf7091cc8df85d7e869770948
Mahmoud Mahfouz <mahmoudmahfouz123@gmail.com>
2023-11-07 00:00:35 -0800

    init

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+  * init_dog - Initializes a dog structure
+  * @d: A dog structure.
+  * @name:name
+  * @age: age
+  * @owner: owner
+  * Return: Nothing
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
