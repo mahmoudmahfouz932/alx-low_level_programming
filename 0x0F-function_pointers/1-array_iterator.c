5c28483d4c4329578552e6f4aa81016490f3e343
Mahmoud Mahfouz <mahmoudmahfouz123@gmail.com>
2023-11-08 04:40:29 -0800

    fuction as arrgument

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+  * array_iterator - array
+  * @array: array
+  * @size: size
+  * @action: action
+  *
+  * Return: void
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (array != NULL && action != NULL && size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
